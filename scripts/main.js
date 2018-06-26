69646effffe447320c871ea27b6913eb861527e6
Ryan Otto <rotto@drew.edu>
2018-06-26 11:11:53 -0400

    Add functional previous/next buttons for Gigi speech

--- scripts/main.js
+++ scripts/main.js
@@ -9,13 +9,41 @@ $(document).ready(function() {
 		}
 	);
 
+	$('#previous').click(gigiPreviousDialogue);
+	$('#next').click(gigiNextDialogue);
+
 	var gigiSpeechIndex=0;
 	var gigiSpeechArray=['Welcome to our laundromat!','This is a second message!','And a third!','And a fourth!'];
-	    setInterval(function() {
+	var gigiSpeechTimer = setInterval(gigiSpeechCycle, 5000);
+
+	function gigiSpeechCycle() {
+	  	if(gigiSpeechIndex > gigiSpeechArray.length-1){ gigiSpeechIndex=0; }
+       	$('#gigiSpeech').fadeTo('1000', 0, function() { $('#gigiSpeech').text(gigiSpeechArray[gigiSpeechIndex-1]);});
+		$('#gigiSpeech').fadeTo('1000', 1);
+		gigiSpeechIndex++;
+					console.log(gigiSpeechIndex);
+
+   	}
+
+	    function gigiNextDialogue(){
 	    	if(gigiSpeechIndex > gigiSpeechArray.length-1){ gigiSpeechIndex=0; }
+	    	gigiSpeechIndex++;
         	$('#gigiSpeech').fadeTo('1000', 0, function() { $('#gigiSpeech').text(gigiSpeechArray[gigiSpeechIndex-1]);});
 			$('#gigiSpeech').fadeTo('1000', 1);
-			gigiSpeechIndex++;
+			clearInterval(gigiSpeechTimer);
+			gigiSpeechTimer = setInterval(gigiSpeechCycle, 5000);
+			console.log(gigiSpeechIndex);
+	    }
 
-    }, 5000);
+
+	    function gigiPreviousDialogue(){
+	    	gigiSpeechIndex--;
+	    	if(gigiSpeechIndex <= 0){ gigiSpeechIndex=gigiSpeechArray.length; }
+        	$('#gigiSpeech').fadeTo('1000', 0, function() { $('#gigiSpeech').text(gigiSpeechArray[gigiSpeechIndex-1]);});
+			$('#gigiSpeech').fadeTo('1000', 1);
+			clearInterval(gigiSpeechTimer);
+			gigiSpeechTimer = setInterval(gigiSpeechCycle, 5000);
+						console.log(gigiSpeechIndex);
+
+	    }
 });
