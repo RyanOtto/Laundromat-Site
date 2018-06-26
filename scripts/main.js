afb968f2d075774cc41634157580008eeb6f0ffa
Ryan Otto <rotto@drew.edu>
2018-06-26 00:56:41 -0400

    Add navbar text popout animation, fix non-index page navbar html

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
 	
+	$(".navbar-item").hover(function() {
+		  $( this ).animate({"font-size":"120%"}, 100);
+	},
+	function(){
+		$( this ).animate({"font-size":"100%"}, 100);
+	}
+	);
 });
